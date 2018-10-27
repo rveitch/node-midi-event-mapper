786293d39bb3da87ca9e617f57ba8359fed1c677
ryan.veitch <ryan.r.veitch@gmail.com>
2018-10-27 15:50:52 -0500

    [CHORE] Adds list-devices-script

---
+++ scripts/list-devices.js
@@ -0,0 +1,13 @@
+/* eslint-disable new-cap, no-console */
+const _ = require('lodash');
+const Midi = require('midi');
+
+function getPortNames() {
+  const MidiIn = new Midi.input();
+  const portCount = MidiIn.getPortCount();
+  const portNames = _.times(portCount, (port) => MidiIn.getPortName(port));
+  _.forEach(portNames, (portName, portNumber) => console.log(`Port ${portNumber}: ${portName}`));
+  return portNames;
+}
+
+getPortNames();
