30d93ea6befdcdfb6f7a0f9769b09a786cc6b928
ryan.veitch <ryan.r.veitch@gmail.com>
2018-10-27 20:07:11 -0500

    [CHORE] Random MODEP patch change on PC recieve

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+const dotenv = require('dotenv').config();
+
 // Libs
 const _ = require('lodash');
 const easymidi = require('easymidi');
 
 const MidiCore = require('./lib/services/midi-core');
 
-/* ****************************** SETUP ***************************** */
-
+/* ****************************** APP ***************************** */
 const defaultDeviceName = MidiCore.getDefaultDeviceName();
 console.info(`Creating Input for Default Device: ${defaultDeviceName}`);
 const MidiIn = new easymidi.Input(defaultDeviceName);
 
 MidiIn.on('message', (params) => {
-  if (_.get(params, '_type') === 'sysex') return;
+  const messageType = _.get(params, '_type');
+  if (messageType === 'sysex') return;
   console.log(params);
+  if (messageType === 'program' && !process.env.LOG_ONLY) MidiCore.changeRandomPreset();
 });
